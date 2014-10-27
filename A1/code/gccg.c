40a72fa6111438d02ff58cc27bdf70d89d884067
Yunqing <eric.dong1234@gmail.com>
2014-10-27 14:45:07 +0100

    PAPI implementation

--- A1/code/gccg.c
+++ A1/code/gccg.c
@@ -9,10 +9,13 @@
 #include <string.h>
 #include <math.h>
 #include "mpi.h"
+#include <papi.h>
 
 #include "initialization.h"
 #include "compute_solution.h"
 #include "finalization.h"
+
+#define NUM_EVENTS 5
 
 int main(int argc, char *argv[]) {
     int i;
@@ -57,15 +60,47 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Failed to initialize data!\n");
         abort();
     } 
-
-   
     /********** END INITIALIZATION **********/
 
+    /**********Initialize the PAPI Counters************/
+    float rtime, ptime, mflops;
+    long long flpops;
+     
+    long long counters[NUM_EVENTS];
+    int PAPI_events[] = {
+        PAPI_L2_TCM, /*Level 2 cache misses*/
+        PAPI_L2_TCA, /*Level 2 total cache accesses*/
+        PAPI_L3_TCM, /*Level 3 cache misses*/
+        PAPI_L3_TCA, /*Level 3 total cache accesses*/
+        PAPI_TOT_CYC, /*Total cycles*/};
+
+    if(PAPI_library_init(PAPI_VER_CURRENT) != PAPI_VER_CURRENT) exit(1);
+    
+    /**********Start Counting*********/
+    if(PAPI_start_counters(PAPI_events,NUM_EVENTS) != PAPI_OK) handle_error(1);
+    if(PAPI_flops( &rtime, &ptime, &flpops,  &mflops ) != PAPI_OK) handle_error(1); 
+        
+
     /********** START COMPUTATIONAL LOOP **********/
+    
+    
     int total_iters = compute_solution(max_iters, nintci, nintcf, nextcf, lcc, bp, bs, bw, bl, bn,
                                        be, bh, cnorm, var, su, cgup, &residual_ratio);
     /********** END COMPUTATIONAL LOOP **********/
+    
+    /*********stop counting *********/
+    if(PAPI_flops( &rtime, &ptime, &flpops,  &mflops ) != PAPI_OK) handle_error(1); 
+    if(PAPI_stop_counters(counters,NUM_EVENTS) != PAPI_OK) handle_error(1);
 
+    printf("the execution time is %f. \n", rtime);
+    
+    printf("the Mflops is %f.", mflops);
+    
+    printf("%lld L2 cache misses in %lld cycles, with L2 cash miss rate %.3lf \n", PAPI_events[0], PAPI_events[4], (double)PAPI_events[0]/PAPI_events[1] );
+    
+    printf("%lld L3 cache misses in %lld cycles, with L3 cash miss rate %.3lf \n", PAPI_events[2], PAPI_events[4], (double)PAPI_events[2]/PAPI_events[3] );
+    
+    
     /********** START FINALIZATION **********/
     finalization(file_in, total_iters, residual_ratio, nintci, nintcf, var, cgup, su);
     /********** END FINALIZATION **********/
