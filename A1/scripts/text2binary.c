491245508718ba2880843127036c23bf97456c3e
Mariusz Bujny <mario1701@tlen.pl>
2014-10-27 13:31:49 +0100

    Create text2binary tool

---
+++ A1/scripts/text2binary.c
@@ -0,0 +1,134 @@
+/**
+ * Helper functions for reading from input data file
+ *
+ * @author E. Xue, V. Petkov
+ * @date 22-May-2009, 22-Oct-2012
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "util_read_files.h"
+
+int main(int *argc, char *argv[])
+{
+  int i;
+  
+  char *filename;
+  int *nintci, *nintcf, *nextci, *nextcf, ***lcc;
+  double **bs, **be, **bn, **bw, **bl, **bh, **bp, **su;
+  
+  // Part of the code from util_read_files.c
+  //=================================================================================
+  FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        printf("Error opening file %s\n", filename);
+        return -1;
+    }
+    //4 variables in total!!!
+    fscanf(fp, "%d", nintci);
+    fscanf(fp, "%d", nintcf);
+    fscanf(fp, "%d", nextci);
+    fscanf(fp, "%d", nextcf);
+
+    //allocating lcc
+    if ((*lcc = (int**) malloc( ( (*nintcf) - (*nintci) + 1) * sizeof(int*) )) == NULL)
+    {
+        printf("malloc failed to allocate first dimension of lcc (nintcf)");
+        return -1;
+    }
+    for (i = (*nintci); i <= (*nintcf); i++)
+    {
+        if (((*lcc)[i] = (int *) malloc( 6 * sizeof(int) )) == NULL)
+        {
+            printf("malloc(lcc) failed\n");
+            return -1;
+        }
+    }
+
+    //start reading lcc
+    //note that c array index starts from 0 while fortran starts from 1!
+    for (i = (*nintci); i <= (*nintcf); i++)
+    {
+        fscanf(fp, "%d", &(*lcc)[i][0]);
+        fscanf(fp, "%d", &(*lcc)[i][1]);
+        fscanf(fp, "%d", &(*lcc)[i][2]);
+        fscanf(fp, "%d", &(*lcc)[i][3]);
+        fscanf(fp, "%d", &(*lcc)[i][4]);
+        fscanf(fp, "%d", &(*lcc)[i][5]);
+    }
+
+    // allocate other arrays
+    if ((*bs = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*be = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*bn = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*bw = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*bl = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*bh = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*bp = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+    if ((*su = (double *) malloc((*nextcf + 1) * sizeof(double))) == NULL)
+    {
+        printf("malloc() failed\n");
+        return -1;
+    }
+
+    // read the other arrays
+    for (i = (*nintci); i <= *nintcf; i++)
+    {
+        fscanf(fp, "%lf", &((*bs)[i]));
+        fscanf(fp, "%lf", &((*be)[i]));
+        fscanf(fp, "%lf", &((*bn)[i]));
+        fscanf(fp, "%lf", &((*bw)[i]));
+        fscanf(fp, "%lf", &((*bl)[i]));
+        fscanf(fp, "%lf", &((*bh)[i]));
+        fscanf(fp, "%lf", &((*bp)[i]));
+        fscanf(fp, "%lf", &((*su)[i]));
+    }
+
+    fclose(fp);
+  //=================================================================================
+    
+  // Freeing the memory
+  free(su);
+  free(bp);
+  free(bh);
+  free(bl);
+  free(bw);
+  free(bn);
+  free(be);
+  free(bs);
+  
+  for (i=0; i < ( (*nintcf) - (*nintci) + 1); i++)
+  {
+    free(lcc[i]);
+  }
+  free(lcc);
+  
+}
